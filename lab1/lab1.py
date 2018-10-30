64a8982c176621684f6bd693adfb71077cbcc879
MikeMadeira <michael-madeira@hotmail.com>
2018-10-30 15:25:55 +0000

    update

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -123,14 +123,59 @@ AmmoniumNorm2 = Ammonium / np.linalg.norm(Ammonium)
 
 # print "Normalizacao ", np.all(pHnorm2 == pHnorm) 
 
-plt.figure()
-plt.scatter(pHnorm2,AmmoniumNorm2)
+# plt.figure()
+# plt.scatter(pHnorm2,AmmoniumNorm2)
 
-plt.figure()
-plt.scatter(pHnorm,AmmoniumNorm)
+# plt.figure()
+# plt.scatter(pHnorm,AmmoniumNorm)
+# plt.show()
+
+# print data.head()
+# print np.cov(data.loc[:,['pH','Oxygen','Chloride','Nitrates','Ammonium','Orthophosphate','Phosphate','Chlorophyll']])
+
+attributes = ['pH','Oxygen','Chloride','Nitrates','Ammonium','Orthophosphate','Phosphate','Chlorophyll']
+
+def plotAttributes(att_name1,att_name2):
+
+	att1 = data[att_name1]
+	att2 = data[att_name2]
+
+	plt.figure()
+	plt.scatter(att1,att2)
+	plt.xlabel(att_name1)
+	plt.ylabel(att_name2)
+	plt.show()
+
+
+
+def normalize(att1,att2):
+
+	att1norm = (att1 - att1.min())/(att1.max() - att1.min())
+	att2norm = (att2 - att2.min())/(att2.max() - att2.min())
+
+	return att1norm,att2norm
+
+def exerciciog(attributes):
+
+	for i in range(len(attributes)):
+		for j in range(i,len(attributes)):
+
+			att1 = data[attributes[i]]
+			att2 = data[attributes[j]]
+
+			att1norm,att2norm = normalize(att1,att2)
+
+			att_conc_corr = data[[attributes[i],attributes[j]]].corr()
+			print att_conc_corr
+
+			plt.figure()
+			plt.scatter(att1norm,att2norm)
+			plt.xlabel(attributes[i])
+			plt.ylabel(attributes[j])
+			plt.show()
+
+# exerciciog(attributes)
+# plotAttributes('pH','Chlorophyll')
+
+data['Chlorophyll'].hist()
 plt.show()
-
-print data.head()
-print np.cov(data.loc[:,['pH','Oxygen','Chloride','Nitrates','Ammonium','Orthophosphate','Phosphate','Chlorophyll']])
-
-

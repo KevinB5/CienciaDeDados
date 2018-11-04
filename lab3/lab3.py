32f1802402410651c4eae7dc83401e2caa803f63
MikeMadeira <michael-madeira@hotmail.com>
2018-11-04 18:10:27 +0000

    update lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -56,16 +57,49 @@ def conf_matrix_sens_spec(Y_test,predict):
     specificity = tn*1./(tn+fp)
     print 'Specificity ',specificity
 
-def roc_curve_func(Y_test,predict):
+def roc_curve_func(Y_test,predict,roc_color):
 
     fpr, tpr, _ = roc_curve(Y_test,predict)
     roc_auc = auc(fpr,tpr)
 
     plt.figure()
-    plt.plot(fpr,tpr, color='darkorange',lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot(fpr,tpr, color=roc_color,lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.legend(loc="lower right")
+
+def cross_val(X,Y,classifier,roc_color):
+
+    accuracies_test = []
+    k=1
+
+    #Split the data with k folds for train and the rest to test 
+    skf = StratifiedKFold(n_splits = 3, random_state = None, shuffle = True)
+    for train_index, test_index in skf.split(X,Y):
+        X_train, X_test = X.loc[train_index], X.loc[test_index]
+        Y_train, Y_test = Y.loc[train_index], Y.loc[test_index]
+        #Trainning the model
+        model = classifier.fit(X_train,Y_train)
+        #Prediction  
+        Y_predict = model.predict(X_test)
+        #Plot the model
+        # plt_Prediction_Cross_Validation(Y_test,Y_predict,k)
+        #Accuracy
+        accuracy_test = accuracy_score(Y_test, Y_predict)
+        #Put all accuricies in array to calculate mean accuracy
+        accuracies_test.append(accuracy_test)
+        #Count KFold
+        k+=1
+        #Calculate Confusion Matrix
+        conf_matrix = confusion_matrix(Y_test, Y_predict)
+        #Show Confusion Matrix
+        plot_confusion_matrix(conf_matrix,classes)
+
+        conf_matrix_sens_spec(Y_test,Y_predict)
+
+        roc_curve_func(Y_test,predict,roc_color)
+
+    return np.mean(accuracies_test),np.std(accuracies_test)
 
 
 
@@ -117,22 +151,22 @@ def roc_curve_func(Y_test,predict):
 ########## Exercicio3
 
 unbalanced_data = pd.read_csv('unbalanced.csv')
-unbalanced_data = preprocessData(unbalanced_data)
+# unbalanced_data = preprocessData(unbalanced_data)
 
 X = unbalanced_data.iloc[:,:-1]
 # print X
 Y = unbalanced_data['Outcome']
 # print Y
 
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y)
+classifier1 = GaussianNB()
 
-predict = naive_Bayes(X_train,X_test,Y_train)
+classifier2 = KNeighborsClassifier(n_neighbors = 1)
 
-accuracy = accuracy_score(Y_test,predict)
+classifier3 = KNeighborsClassifier(n_neighbors = 10)
 
-print 'Accuracy ', accuracy
+classifier4 = KNeighborsClassifier(n_neighbors = 100)
 
-conf_matrix_sens_spec(Y_test,predict)
+
 
 # roc_curve_func(Y_test,predict,'darkorange')
 

aa948ffda7704f5204aff6e3c27d51de5c3b940f
Kevin <kevin-batistac@hotmail.com>
2018-11-06 14:34:19 +0000

    Pré-processamento inicio

--- projecto/projeto.py
+++ projecto/projeto.py
@@ -10,12 +10,32 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 
-#Leitura dos dados
+#Leitura dos dados Digital Colposcopies
 green = pd.read_csv('C:/Users/kevin/Desktop/IST/Ciencia de Dados/projecto/Quality Assessment-Digital Colposcopy/green.csv')
 hinselmann = pd.read_csv('C:/Users/kevin/Desktop/IST/Ciencia de Dados/projecto/Quality Assessment-Digital Colposcopy/hinselmann.csv')
 schiller = pd.read_csv('C:/Users/kevin/Desktop/IST/Ciencia de Dados/projecto/Quality Assessment-Digital Colposcopy/schiller.csv')
 
+#Verificar se o formato é utilizável 
+green.head()
+hinselmann.head()
+schiller.head()
+
+#Remover os dados vazios
+green = green.replace(0.0,np.nan)
+green = green.dropna()
+print green.isnull().sum()
+
+hinselmann = hinselmann.replace(0.0,np.nan)
+hinselmann = hinselmann.dropna()
+print hinselmann.isnull().sum()
+
+schiller = schiller.replace(0.0,np.nan)
+schiller = schiller.dropna()
+print schiller.isnull().sum()
+
+#Leitura dos dados APS failure at Scania trucks
 aps_test = pd.read_csv('C:/Users/kevin/Desktop/IST/Ciencia de Dados/projecto/aps_failure/aps_failure_test_set.csv')
 aps_training = pd.read_csv('C:/Users/kevin/Desktop/IST/Ciencia de Dados/projecto/aps_failure/aps_failure_training_set.csv')
+
 
 

e356a8e3895914b0cf3a06ee9b367023647e8b4c
MikeMadeira <michael-madeira@hotmail.com>
2018-11-14 18:48:30 +0000

    update metrics

--- projecto/metrics.py
+++ projecto/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report,roc_curve, auc
 
 accuracies = []
 errors = []

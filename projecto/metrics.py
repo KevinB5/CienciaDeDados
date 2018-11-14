a8b1d77b4482994ff939504612718f334eb6ce0b
MikeMadeira <michael-madeira@hotmail.com>
2018-11-14 09:44:36 +0000

    update metrics

--- projecto/metrics.py
+++ projecto/metrics.py
@@ -41,3 +41,65 @@ def error(prediction,Ytest):
 def classi_report(prediction,Ytest):
 
 	print classification_report(Ytest,prediction)
+
+def conf_matrix_sens_spec(Y_test,predict):
+
+    conf_matrix = confusion_matrix(Y_test,predict)
+    tn, fp, fn, tp = confusion_matrix(Y_test,predict).ravel()
+    print 'Confusion Matrix '
+    print conf_matrix
+
+    sensitivity = tp*1./(tp+fn)
+    print 'Sensitivity ',sensitivity
+
+    specificity = tn*1./(tn+fp)
+    print 'Specificity ',specificity
+
+def plot_confusion_matrix(cnf_matrix, classesNames, normalize=False, cmap=plt.cm.Blues):
+    """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+    """
+    np.set_printoptions(precision=2)
+
+    if normalize:
+        soma = cnf_matrix.sum(axis=1)[:, np.newaxis]
+        cm = cnf_matrix.astype('float') / soma
+        title = "Normalized confusion matrix"
+    else:
+        cm = cnf_matrix
+        title = 'Confusion matrix, without normalization'
+
+    plt.figure()
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classesNames))
+    plt.xticks(tick_marks, classesNames, rotation=45)
+    plt.yticks(tick_marks, classesNames)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+def roc_curve_func(Y_test,predict,roc_color,title):
+
+    fpr, tpr, _ = roc_curve(Y_test,predict)
+    roc_auc = auc(fpr,tpr)
+
+    # plt.figure()
+    plt.plot(fpr,tpr, color=roc_color,lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc="lower right")
+    plt.show()
